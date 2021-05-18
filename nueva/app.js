558c2beb8664faf013fcc1e85a9a440a83b1ca7e
Aldo <aldoyedro181@gmail.com>
2021-05-18 19:04:45 -0300

    Fixing Menu JS

--- nueva/app.js
+++ nueva/app.js
@@ -3,10 +3,4 @@ const navMenu = document.querySelector(".navigation");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav_hidden");
-
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
 });
